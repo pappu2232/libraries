7dcb49a5b70bb0859124bbea637de1c610022463
pappu2232 <162913749+pappu2232@users.noreply.github.com>
2024-03-25 20:52:43 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -2,14 +2,17 @@ def sum(a,b)
 {
    echo "the sum of ${a] and ${b} is ${a+b}"
 }
+   
 def sub(a,b)
 {
    echo "the substraction of ${a} and ${b} is ${a-b}"
 }
+   
 def mul(a,b)
 {
    echo "the multipication of ${a} and ${b} is ${a*b}"
 }
+   
 def div(a,b)
 {
    echo "the division of ${a} and ${b} is ${a/b}"
